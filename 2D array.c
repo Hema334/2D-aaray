b8207059535ba2766fd74e4e35b6739530cb6158
Hema334 <112267828+Hema334@users.noreply.github.com>
2024-09-09 20:58:16 +0530

    Add files via upload

---
+++ 2D array.c
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdio.h>
+void main()
+{
+int arr[1][2] ,i,j;
+  for(i=0;i<2;i++)
+  {
+  for(j=0;j<2;j++)
+  {
+      printf("enter element:");
+      scanf("%d",&arr[i][j]);
+  }
+  }
+  for(i=0;i<2;i++)
+  {
+  for(j=0;j<2;j++)
+  {
+      printf("enter element:");
+      scanf("/n");
+  }
+  }
+}
+  
